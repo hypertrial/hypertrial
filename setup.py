22bf22cb6a2d27e6f70070bee68542a9c91c15fb
Matt Faltyn <faltyn.matthew@gmail.com>
2025-04-09 12:28:11 +0200

    Fixed failing tests

--- setup.py
+++ setup.py
@@ -36,11 +36,14 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11", 
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Topic :: Office/Business :: Financial :: Investment",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.6, <3.13",
     entry_points={
         "console_scripts": [
             "hypertrial=core.main:main",
