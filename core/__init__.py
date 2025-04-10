f5489a4a76ef9fb061e7a73597cd6929ce46b82e
Matt Faltyn <faltyn.matthew@gmail.com>
2025-04-10 08:37:27 +0200

    0.1.4

--- core/__init__.py
+++ core/__init__.py
@@ -17,7 +17,7 @@ Official repository: https://github.com/mattfaltyn/hypertrial
 PyPI package: https://pypi.org/project/hypertrial/
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 # Make the security module available at the package level
 from core import security
